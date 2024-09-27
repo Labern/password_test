36bdcdb2f14f715df33d1b20481a16a3c85e9648
Labern <lukelabern@gmail.com>
2024-09-27 02:43:21 +0100

    Tweaks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    # If session is nil, set it to nil, or find the user via the ID stored in the session.
     @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
   end
 
